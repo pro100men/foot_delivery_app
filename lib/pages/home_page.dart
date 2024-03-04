beea709ae198e75b8d285ea2701b45edffeec924
pro100men <adik.kyyal@gmail.com>
2024-03-04 20:03:44 +0600

    change code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,103 +12,106 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: [
-          AppBarWidget(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-            child: Container(
-              width: double.infinity,
-              height: 50,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 10,
-                      offset: Offset(0, 3)),
-                ],
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Icon(
-                      CupertinoIcons.search,
-                      color: Colors.red,
-                    ),
-                    Container(
-                      width: 330,
-                      height: 50,
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 15),
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            hintText: 'Поиск товаров',
-                            border: InputBorder.none,
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          children: [
+            AppBarWidget(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: Container(
+                width: double.infinity,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 10,
+                        offset: Offset(0, 3)),
+                  ],
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(
+                        CupertinoIcons.search,
+                        color: Colors.red,
+                      ),
+                      Container(
+                        width: 330,
+                        height: 50,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 15),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              hintText: 'Поиск товаров',
+                              border: InputBorder.none,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Icon(Icons.filter_list)
-                  ],
+                      Icon(Icons.filter_list)
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 20, left: 10),
-            child: Text(
-              'Категории',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 10),
+              child: Text(
+                'Категории',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
             ),
-          ),
-          CategoriesWidget(),
-          Padding(
-            padding: EdgeInsets.only(top: 20, left: 10),
-            child: Text(
-              'Популярное',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            CategoriesWidget(),
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 10),
+              child: Text(
+                'Популярное',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
             ),
-          ),
-          PopularItemWidget(),
-          Padding(
-            padding: EdgeInsets.only(top: 20, left: 10),
-            child: Text(
-              'Новинки',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            PopularItemWidget(),
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 10),
+              child: Text(
+                'Новинки',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
             ),
-          ),
-          NewestItemWidget(),
-        ],
-      ),
-      drawer: DrawerWidget(),
-      floatingActionButton: Container(
-        decoration:
-            BoxDecoration(borderRadius: BorderRadius.circular(20), boxShadow: [
-          BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 2,
-              blurRadius: 10,
-              offset: Offset(0, 3))
-        ]),
-        child: FloatingActionButton(
-          onPressed: () {
-            Navigator.pushNamed(context, "cartPage");
-          },
-          child: Icon(
-            CupertinoIcons.cart,
-            size: 25,
-            color: Colors.red,
-          ),
-          backgroundColor: Colors.white,
+            NewestItemWidget(),
+          ],
         ),
+        drawer: DrawerWidget(),
+        floatingActionButton: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: Offset(0, 3))
+              ]),
+          child: FloatingActionButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "cartPage");
+            },
+            child: Icon(
+              CupertinoIcons.cart,
+              size: 25,
+              color: Colors.red,
+            ),
+            backgroundColor: Colors.white,
+          ),
+        ),
+        bottomNavigationBar: HomeBottomNavBar(),
       ),
-      bottomNavigationBar: HomeBottomNavBar(),
     );
   }
 }
